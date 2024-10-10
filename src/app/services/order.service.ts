49a3181de11d7759ce6da8d1713bb9d7f18a5838
afisovvlad <afisovvlad@gmail.com>
2024-10-10 13:02:27 +0300

    ready

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {DataOrderType} from "../../types/dataOrder.type";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from 'rxjs';
+import {ResponseOrder} from '../../types/responseOrder.type';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
-  orderRequest(data: DataOrderType): Observable<ResponseType> {
-    return this.http.post<ResponseType>('https://testologia.ru/order-tea', data);
+  orderRequest(data: DataOrderType): Observable<ResponseOrder> {
+    return this.http.post<ResponseOrder>('https://testologia.ru/order-tea', data);
   }
 }
